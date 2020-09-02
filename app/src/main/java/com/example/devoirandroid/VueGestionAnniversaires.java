14c3971bcb47c6780da7725421ba17688f1d867c
KennyMarechal <44245153+KennyMarechal@users.noreply.github.com>
2020-09-02 18:45:26 +0400

    Preparation du devoir android
    Premier commit pour préparer le devoir android tout est fonctionel

---
+++ app/src/main/java/com/example/devoirandroid/VueGestionAnniversaires.java
@@ -0,0 +1,77 @@
+package com.example.devoirandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class VueGestionAnniversaires extends AppCompatActivity {
+
+    protected ListView vueGestionAnniversaires;
+    protected List<HashMap<String, String>> listeAnniversaire;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vue_gestion_anniversaires);
+
+        vueGestionAnniversaires = (ListView)findViewById(R.id.vueListeGestionAnniversaires);
+
+        listeAnniversaire = preparerListeAnniversaire();
+
+        SimpleAdapter adapter = new SimpleAdapter(
+                this,
+                listeAnniversaire,
+                android.R.layout.two_line_list_item,
+                new String[] {"titre", "date de realisation", "heure","description","url"},
+                new int[] {android.R.id.text1, android.R.id.text2});
+
+        vueGestionAnniversaires.setAdapter(adapter);
+    }
+
+    public List<HashMap<String,String>> preparerListeAnniversaire(){
+
+        List<HashMap<String,String>> listeAnniversaire = new ArrayList<HashMap<String,String>>();
+
+        HashMap<String, String> anniversaire;
+
+        anniversaire = new HashMap<String,String>();
+        anniversaire.put("titre", "anniversaire Maman");
+        anniversaire.put("date de realisation", "02/09/2020");
+        anniversaire.put("heure", "18:16");
+        anniversaire.put("description ", "pas de description");
+        anniversaire.put("url", "no link");
+        listeAnniversaire.add(anniversaire);
+
+        anniversaire = new HashMap<String,String>();
+        anniversaire.put("titre", "anniversaire papa");
+        anniversaire.put("date de realisation", "02/09/2020");
+        anniversaire.put("heure", "18:16");
+        anniversaire.put("description ", "pas de description");
+        anniversaire.put("url", "no link");
+        listeAnniversaire.add(anniversaire);
+
+        anniversaire = new HashMap<String,String>();
+        anniversaire.put("titre", "anniversaire Leon");
+        anniversaire.put("date de realisation", "02/09/2020");
+        anniversaire.put("heure", "18:16");
+        anniversaire.put("description ", "pas de description");
+        anniversaire.put("url", "no link");
+        listeAnniversaire.add(anniversaire);
+
+        anniversaire = new HashMap<String,String>();
+        anniversaire.put("titre", "anniversaire Kenny");
+        anniversaire.put("date de realisation", "02/09/2020");
+        anniversaire.put("heure", "18:16");
+        anniversaire.put("description ", "pas de description");
+        anniversaire.put("url", "no link");
+        listeAnniversaire.add(anniversaire);
+
+        return listeAnniversaire;
+    }
+}
